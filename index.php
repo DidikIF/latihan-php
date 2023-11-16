55c152f37185dbb329cc71240826b7fc2fc9564d
DidikIF <dfauzi72@gmail.com>
2023-11-16 21:08:55 +0700

    tugasphp1

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+            Belajar PHP
+        </title>
+    <head>
+    <body>
+    <h1>
+        Selamat datang di dunia <?php echo "masukkan nama anda",?>
+    </h1>
+    </body>
+<html>
